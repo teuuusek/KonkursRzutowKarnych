b3dd0ebe0bcf1cb9b95ec8003bc0f6678b62c376
teuuusek <mateuszmatynia@wp.pl>
2025-12-02 23:58:09 +0100

    main: dodano statyczną metodę wypiszNaglowek w klasie Main
    Rozszerzono klasę Main o statyczną metodę wypiszNaglowek(), która wypisuje prosty nagłówek programu.

--- src/konkursrzutowkarnychMain.java
+++ src/konkursrzutowkarnychMain.java
@@ -3,7 +3,13 @@ package konkursrzutowkarnych;
 import java.until.Scanner;
 
 public class Main {
+
+  public static void wypiszNaglowek() {
+    System.out.println("================================");
+    System.out.println("     KONKURS RZUTÓW KARNYCH     ");
+    System.out.println("================================");
   public static void main(String[] args) {
+    wypiszNaglowek();
     Scanner scanner = new Scanner(System.in);
     ZapisWynikow zapisWynikow = new ZapisyWynikow("wyniki.txt);
     MenedzerTurnieju menedzer = new MenedzerTurnieju(scanner, zapisWynikow);
